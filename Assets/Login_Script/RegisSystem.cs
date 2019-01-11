246dabf1fb6914897d0af87d9df7f74d06bea95e
sueshi <suekozuu@gmail.com>
2019-01-11 12:16:19 +0700

    [ADD] Regis Script in Login

---
+++ Assets/Login_Script/RegisSystem.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Text.RegularExpressions;
+
+public class RegisSystem : MonoBehaviour
+{
+    public GameObject username;
+    public GameObject password;
+    public GameObject confPassword;
+    private string Username;
+    private string Password;
+    private string ConfPassword;
+    private string form;
+    private bool EmailValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void RegisButton()
+    {
+        bool UN = false;
+        bool PW = false;
+        bool CPW = false;
+
+        if (Username != "")
+        {
+            if (!System.IO.File.Exists(@"" + Username + ".txt"))
+            {
+                UN = true;
+            }
+            else
+            {
+                Debug.LogWarning("Username Taken");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username field empty");
+        }
+        if (Password != "")
+        {
+            if(Password.Length > 5)
+            {
+                PW = true;
+            }
+            else
+            {
+                Debug.LogWarning("Password Must Be atleast 6 characters long");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password field empty");
+        }
+        if (ConfPassword != "")
+        {
+            if (ConfPassword == Password)
+            {
+                CPW = true;
+            }
+            else
+            {
+                Debug.LogWarning("Passwords don't Match");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Confirm Password field empty");
+        }
+        if (UN == true && PW == true && CPW == true)
+        {
+            bool Clear = true;
+            int i = 1;
+            foreach(char c in Password)
+            {
+                if (Clear)
+                {
+                    Password = "";
+                    Clear = false;
+                }
+                i++;
+                char Encrypted = (char)(c * i);
+                Password += Encrypted.ToString();
+            }
+            form = (Username + "\n" + Password);
+            System.IO.File.WriteAllText(@"" + Username + ".txt", form);
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            confPassword.GetComponent<InputField>().text = "";
+            print("Regis complete!");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (username.GetComponent<InputField>().isFocused)
+            {
+                password.GetComponent<InputField>().Select();
+            }
+            if (password.GetComponent<InputField>().isFocused)
+            {
+                confPassword.GetComponent<InputField>().Select();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Password != "" && Password != "")
+            {
+                RegisButton();
+            }
+        }
+        Username = username.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfPassword = confPassword.GetComponent<InputField>().text;
+    }
+}
