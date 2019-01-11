162908700c63b06d8178bca6690d1fdb162f31f5
sueshi <suekozuu@gmail.com>
2019-01-11 16:10:13 +0700

    [UPDATE]LoginScreen Script

--- Assets/Login_Script/LoginScreen.cs
+++ Assets/Login_Script/LoginScreen.cs
@@ -11,8 +11,8 @@ public class LoginScreen : MonoBehaviour
     public GameObject password;
     private string Username;
     private string Password;
-    private string form;
-    private bool EmailValid = false;
+    private String[] Lines;
+    private string DecryptedPass;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,58 @@ public class LoginScreen : MonoBehaviour
 
     public void LoginButton()
     {
-        //("Success!");
+        bool UN = false;
+        bool PW = false;
+        if (Username != "")
+        {
+            if (System.IO.File.Exists(@"" + Username + ".txt"))
+            {
+                UN = true;
+                Lines = System.IO.File.ReadAllLines(@"" + Username + ".txt");
+            }
+            else
+            {
+                Debug.LogWarning("Username Invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Username field empty");
+        }
+        if(Password !=""){
+            if (System.IO.File.Exists(@"" + Username + ".txt")) {
+                int i = 1;
+                foreach (char c in Lines[2])
+                {
+                    i++;
+                    char Decrypted = (char)(c / i);
+                    DecryptedPass += DecryptedPass.ToString();
+                }
+                if(Password == DecryptedPass)
+                {
+                    PW = true;
+                }
+                else
+                {
+                    Debug.LogWarning("Password is Invalid");
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Password is Invalid");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Password Field Empty");
+        }
+        if (UN = true && PW == true)
+        {
+            username.GetComponent<InputField>().text = "";
+            password.GetComponent<InputField>().text = "";
+            print("Login Sucessful");
+            Application.LoadLevel("Start Menu");
+        }
     }
 
     // Update is called once per frame
