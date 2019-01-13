bbd8c6ce9fa8f060968c5c3543ee30d9b22c523a
Nattawanee <nattawanee.sks@mail.kmutt.ac.th>
2019-01-13 15:00:22 +0700

    [UPDATE]Fight box appear

---
+++ Assets/Home_script/FightboxVisible.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightboxVisible : MonoBehaviour {
+    private bool btnStatus;
+    public GameObject selectBox;
+	// Use this for initialization
+	void Start () {
+        btnStatus = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(btnStatus == false){
+            selectBox.active = false;
+        }
+        if(btnStatus == true){
+            selectBox.active = true;
+        }
+	}
+
+    public void fightboxx(){
+        if (btnStatus == false)
+        {
+            btnStatus = true;
+        }
+        else
+        {
+            btnStatus = false;
+        }
+    }
+}
---
+++ Assets/Link_Scene/goFightLv1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goFightLv1 : MonoBehaviour {
+
+    public static bool clickFight;
+    private Scene gotoFight;
+    void Start()
+    {
+        clickFight = false;
+        gotoFight = SceneManager.GetActiveScene();
+
+    }
+    void Update()
+    {
+        if (clickFight == true)
+        {
+            Application.LoadLevel("Level1");
+
+        }
+        if (clickFight == false)
+        {
+            gotoFight = SceneManager.GetActiveScene();
+        }
+    }
+    public void goFightt()
+    {
+        if (clickFight == false)
+        {
+            clickFight = true;
+        }
+        else
+        {
+            clickFight = false;
+        }
+    }
+}
