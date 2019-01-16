a1c23fdd4bfb6e5c2b8ec5068fcad4cc239b9ab7
Nattawanee <nattawanee.sks@mail.kmutt.ac.th>
2019-01-16 11:27:56 +0700

    [ADD]Monster walk Script

---
+++ Assets/Animation/MonsterWalk.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterWalk : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
